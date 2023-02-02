6dfae34ce0583392f25516ed1fa2f37c898effa4
AlexandeRuelas <122758773+AlexandeRuelas@users.noreply.github.com>
2023-02-02 05:18:21 -0400

    Update Programa.ino

--- Programa.ino
+++ Programa.ino
@@ -1,36 +1,25 @@
-#define BLYNK_TEMPLATE_ID           "TMPLh9K46uSQ"
-#define BLYNK_TEMPLATE_NAME         "Quickstart Device"
-#define BLYNK_DEVICE_NAME           "Quickstart Template"
-#define BLYNK_AUTH_TOKEN            "-uoIKHH2HU2c8o6rRTn7Rs17MHwM4oD8"
+#define BLYNK_TEMPLATE_ID           "TMPLlW8ZFcKk"
 
 #define BLYNK_PRINT Serial
 
-#include <ESP8266WiFi.h>
-#include <BlynkSimpleEsp8266.h>
-#include <Servo.h>
 
-int servo_pin = D7;  
-char ssid[] = "ma3damiCNM";
-char pass[] = "chimpitaCNM8891";
+#include <ESP8266WiFi.h>
+
+char ssid[] = "***";
+
+
 
 Servo servo;
 
-BLYNK_WRITE(V1)// El pin virtual de la aplicacion
+BLYNK_WRITE(V0)// El pin virtual de la aplicacion
 {
-  int pinValue = param.asInt();
-  Serial.print("*****V1 Slider value is: ");
-  Serial.println(pinValue);
   servo.write(param.asInt());
 }
 
 void setup()
 {
-  Serial.begin(115200);
-  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
-  servo.attach(servo_pin);
-}
+  // Debug console
+  Serial.begin(11500);
 
-void loop()
-{
-  Blynk.run();
+
 }
